4ff1acbc3406a7a0c520ed60375a1e17e405c0a5
Greyson <greyson.r.hamilton@gmail.com>
2020-10-25 12:44:21 -0400

    begin

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,27 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react';
+
+const Character = () => {
+
+    const [id, setId] = useState();
+
+    useEffect((() => {
+
+        axios.get('https://swapi.dev/api/people/')
+
+        .then((res) => {
+            console.log(res.data.results);
+            setId(res.data.results);
+        })
+
+        .catch((err) => {
+            console.log('This is not the code you are looking for.', err);
+        });
+    }, []));
+
+    return (
+        <div className="character">
+            
+        </div>
+    )
+}
